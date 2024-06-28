ad07d26b2d119cb0a7420456d0f283ce2c0e3943
Eduardo Augusto <eduardo.afreis@gmail.com>
2024-06-28 11:24:21 -0300

    Validação feita com yup para create user, e password_hash funcionando

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,32 @@
 const Users = require("../models/Users");
+const yup = require("yup");
 
 class UserController{
 
   async create(request, response){
+    const schema = yup.object().shape({
+      name: yup
+      .string()
+      .required("O nome é obrigatório")
+      .min(3,"O nome deve ter pelo menos 3 caracteres")
+      .max(50, "O nome deve ter no máximo 50 caracteres"),
+      email: yup
+      .string()
+      .email("O email deve ser válido")
+      .required("O email é obrigatório"),
+      user_name: yup
+      .string()
+      .required("O user_name é obrigatório"),
+      password: yup
+      .string()
+      .required("A senha é obrigatória")
+      .min(6, "A senha deve conter no mínimo 6 caracteres"),
+    })
+
     try {
-      const { email, name, user_name, password_hash} = request.body;
+      await schema.validate(request.body, {abortEarly: false});
+
+      const { email, name, user_name, password} = request.body;
 
       const checkUserExists = await Users.findOne({
         where:{
@@ -16,11 +38,22 @@ class UserController{
         return response.status(400).json({message: "Esse usuário já existe."});
       }
   
-      const user = await Users.create({ name, user_name, email, password_hash});
+      const user = await Users.create({ name, user_name, email, password});
   
-      return response.status(201).json({ user });
+      return response.status(201).json({ user: {
+        id: user.id,
+        name: user.name,
+        user_name: user.user_name,
+        email: user.email,
+        updatedAt: user.updatedAt,
+        createdAt: user.createdAt,
+        password_hash: user.password_hash
+      }});
 
     } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        return response.status(400).json({ errors: error.errors });
+      }
       console.log(error);
       return response.status(500).json({ message: "Erro interno do servidor." });
     }
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 require("./database");
 const express = require("express");
-const app = express();
 const routes = require("./routes");
 
+const app = express();
 app.use(express.json());
 app.use(routes);
 
