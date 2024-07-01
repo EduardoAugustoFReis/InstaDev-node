72a7d2c22339b30dcdc7904e9d2049351cb284f4
Eduardo Augusto <eduardo.afreis@gmail.com>
2024-07-01 15:42:08 -0300

    Refatoração do método de criar usuário feita, inserção de imagem feita.

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -4,52 +4,67 @@ const bcrypt = require("bcryptjs");
 
 class UserController{
 
-  async create(request, response){
+  async create(request, response) {
     const schema = yup.object().shape({
       name: yup
-      .string()
-      .required("O nome é obrigatório")
-      .min(3,"O nome deve ter pelo menos 3 caracteres")
-      .max(50, "O nome deve ter no máximo 50 caracteres"),
+        .string()
+        .required("O nome é obrigatório")
+        .min(3, "O nome deve ter pelo menos 3 caracteres")
+        .max(50, "O nome deve ter no máximo 50 caracteres"),
       email: yup
-      .string()
-      .email("O email deve ser válido")
-      .required("O email é obrigatório"),
+        .string()
+        .email("O email deve ser válido")
+        .required("O email é obrigatório"),
       user_name: yup
-      .string()
-      .required("O user_name é obrigatório"),
+        .string()
+        .required("O user_name é obrigatório"),
       password: yup
-      .string()
-      .required("A senha é obrigatória")
-      .min(6, "A senha deve conter no mínimo 6 caracteres"),
-    })
+        .string()
+        .required("A senha é obrigatória")
+        .min(6, "A senha deve conter no mínimo 6 caracteres"),
+    });
 
     try {
-      await schema.validate(request.body, {abortEarly: false});
+      await schema.validate(request.body, { abortEarly: false });
 
-      const { email, name, user_name, password} = request.body;
+      const { email, name, user_name, password } = request.body;
 
       const checkUserExists = await Users.findOne({
-        where:{
+        where: {
           email: request.body.email,
         }
-      })
-  
-      if(checkUserExists){
-        return response.status(400).json({message: "Esse usuário já existe."});
+      });
+
+      if (checkUserExists) {
+        return response.status(400).json({ message: "Esse usuário já existe." });
       }
-  
-      const user = await Users.create({ name, user_name, email, password});
-  
-      return response.status(201).json({ user: {
-        id: user.id,
-        name: user.name,
-        user_name: user.user_name,
-        email: user.email,
-        updatedAt: user.updatedAt,
-        createdAt: user.createdAt,
-        password_hash: user.password_hash
-      }});
+
+      let avatar = null;
+      if (request.file) {
+        avatar = request.file.filename;
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 8);
+
+      const user = await Users.create({ 
+        name, 
+        user_name, 
+        email, 
+        password: hashedPassword, 
+        avatar 
+      });
+
+      return response.status(201).json({
+        user: {
+          id: user.id,
+          name: user.name,
+          user_name: user.user_name,
+          email: user.email,
+          avatar: user.avatar,
+          updatedAt: user.updatedAt,
+          createdAt: user.createdAt,
+        }
+      });
 
     } catch (error) {
       if (error instanceof yup.ValidationError) {
@@ -58,8 +73,8 @@ class UserController{
       console.log(error);
       return response.status(500).json({ message: "Erro interno do servidor." });
     }
-   
   };
+
 
   async update(request, response){
       const { 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
+const multer = require("multer");
+const { upload } = require("../configs/upload"); 
+
 const routes = Router();
+const uploadConfig = multer(upload); 
 
 const userController = require("../app/controllers/userController");
 const authMiddleware = require("../app/middlewares/auth");
 
-routes.post("/", userController.create);
+routes.post("/", uploadConfig.single("image"), userController.create);
 
 routes.use(authMiddleware);
 routes.put("/", userController.update);
@@ -12,4 +16,3 @@ routes.delete("/", userController.delete);
 routes.get("/", userController.show);
 
 module.exports = routes;
-
